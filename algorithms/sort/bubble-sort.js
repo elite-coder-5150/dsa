a2bc193a45e3d8276ef82b4290d69171ee4e9d99
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-11-17 17:51:43 -0600

    this is the bubble sort algorithm

---
+++ algorithms/sort/bubble-sort.js
@@ -0,0 +1,31 @@
+import { Sort } from "./sort";
+
+export class BubbleSort extends Sort {
+    sort(originalArray) {
+        let swapped = false;
+
+        const arr = [...originalArray];
+
+        for (let i = 1; i < arr.length; i++) {
+            swapped = false;
+
+            this.callbacks.visitingCallback(arr[i]);
+
+            for (let j = 0; j < arr.length - i; j += 1) {
+                this.callbacks.visitingCallback(arr[j]);
+
+                if (this.comparator.lessThan(arr[j + 1], arr[j])) {
+                    [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                return arr
+            }
+        }
+
+        return arr;ß
+    }
+}
